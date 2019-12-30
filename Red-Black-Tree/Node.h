7ef40ba71ab0a57e83a0f3e01a2e512c543e3579
Parsa KamaliPour <parsakamalipour@gmail.com>
2019-12-30 21:58:48 +0330

    starting project

---
+++ Red-Black-Tree/Node.h
@@ -0,0 +1,16 @@
+#ifndef Node_h
+#define Node_h
+
+class Node
+{
+public:
+
+private:
+	int data;
+	Node* left;
+	Node* right;
+
+};
+
+
+#endif // !Node_h
---
+++ Red-Black-Tree/RBTree.h
@@ -0,0 +1,12 @@
+#ifndef RBTree_h
+#define RBTree_h
+
+class RBTree
+{
+public:
+
+private:
+
+};
+
+#endif // !RBTree_h
